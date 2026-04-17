af45325f039381e8b323408505956cc37c0c6992
jolynestarchaser <mrkaokungkub@gmail.com>
2026-04-17 14:56:34 +0700

    Dummy punch V0.9 fix suffix

--- soundscript.js
+++ soundscript.js
@@ -3,12 +3,12 @@ const dmgSound = new Audio("./src/minecraft_hit.mp3");
 const oraOra = new Audio("./src/rapid-ora.mp3");
 const disable = new Audio("./src/disable.mp3");
 const mudaMuda = new Audio("./src/muda.mp3");
-const q1 = new Audio("./src/Yasuo/Yasuo_Original_Q1_3.ogg");
-const q2 = new Audio("./src/Yasuo/Yasuo_Original_Q2_1.ogg");
-const q3 = new Audio("./src/Yasuo/Yasuo_Original_Q3_1.ogg");
-const r = new Audio("./src/Yasuo/Yasuo_Original_R_0.ogg");
+const q1 = new Audio("./src/Yasuo/Yasuo_Original_Q1_3.mp3");
+const q2 = new Audio("./src/Yasuo/Yasuo_Original_Q2_1.mp3");
+const q3 = new Audio("./src/Yasuo/Yasuo_Original_Q3_1.mp3");
+const r = new Audio("./src/Yasuo/Yasuo_Original_R_0.mp3");
 const oraWeapon = new Audio("./src/ora-one-hit.mp3");
-const coolDown = new Audio("./src/deny_cooldown.ogg");
+const coolDown = new Audio("./src/deny_cooldown.mp3");
 
 const slash1 = new Audio("./src/slash1.mp3");
 const slash2 = new Audio("./src/slash2.mp3");
