ede1686c65e7fbecd20b29e5dec28fb7f3f38201
jolynestarchaser <mrkaokungkub@gmail.com>
2026-04-17 15:43:10 +0700

    Dummy Punch V1.0 fix yasuo

--- soundscript.js
+++ soundscript.js
@@ -3,10 +3,10 @@ const dmgSound = new Audio("./src/minecraft_hit.mp3");
 const oraOra = new Audio("./src/rapid-ora.mp3");
 const disable = new Audio("./src/disable.mp3");
 const mudaMuda = new Audio("./src/muda.mp3");
-const q1 = new Audio("./src/Yasuo/yasuo_original_q1_3.mp3");
-const q2 = new Audio("./src/Yasuo/yasuo_original_q2_1.mp3");
-const q3 = new Audio("./src/Yasuo/yasuo_original_q3_1.mp3");
-const r = new Audio("./src/Yasuo/yasuo_original_r_0.mp3");
+const q1 = new Audio("./src/yasuo/yasuo_original_q1_3.mp3");
+const q2 = new Audio("./src/yasuo/yasuo_original_q2_1.mp3");
+const q3 = new Audio("./src/yasuo/yasuo_original_q3_1.mp3");
+const r = new Audio("./src/yasuo/yasuo_original_r_0.mp3");
 const oraWeapon = new Audio("./src/ora-one-hit.mp3");
 const coolDown = new Audio("./src/deny_cooldown.mp3");
 
